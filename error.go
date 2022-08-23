5f4f2367e5086266e9a685eef8f42af5929cd183
shixiaoshan <shixiaoshan@megvii.com>
2022-08-23 16:09:46 +0800

    add error.go

---
+++ error.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/* import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, value := range files {
+		fmt.Println(value.Name())
+	}
+}
+*/
+
+/* //文件写入错误
+
+func proviers(name string) error {
+	if f,
+} */
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module shixiaoshan_go
 
 go 1.17
-
-require (
-	github.com/mdempsky/gocode v0.0.0-20200405233807-4acdcbdea79d // indirect
-	golang.org/x/tools v0.1.11 // indirect
-)
